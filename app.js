dce6609b27f36678538dadb90baa85a10d5adf2f
Haribalan M <its.hari.coder@gmail.com>
2024-12-20 20:32:39 +0530

    test

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ mongoose
 // app.use(cors());
 const corsOptions = {
     origin: 'http://localhost:3000',
-    credentials: true,            //access-control-allow-credentials:true
+    credentials: true,            
     optionSuccessStatus: 200
 }
 app.use(cors(corsOptions));
