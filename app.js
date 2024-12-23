85583a2e05d0c85f5c1dae77ae2c3e4d4c91822d
Fazil <mdnaseemfazil@gmail.com>
2024-12-23 14:53:36 +0530

    cors error fix

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ mongoose
 
 // app.use(cors());
 const corsOptions = {
-    origin: 'http://localhost:3000',
+    origin: 'http://localhost:3001',
     credentials: true,            //access-control-allow-credentials:true
     optionSuccessStatus: 200
 }
