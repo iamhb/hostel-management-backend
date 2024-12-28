5a3bb3084ced105811573daf0defc263b602cd84
Haribalan M <its.hari.coder@gmail.com>
2024-12-28 15:40:01 +0530

    getAll Maintenance API changes

--- src/routes/rRequestMaintenance.js
+++ src/routes/rRequestMaintenance.js
@@ -3,6 +3,7 @@ const mMaintenanceRequest = require('../models/mMaintenanceRequest');
 const mMaintenanceRequestLog = require('../models/mMaintenanceRequestLog');
 const mStaff = require('../models/mStaff');
 const mRoom = require('../models/mRoom');
+const _ = require('lodash');
 
 const router = express.Router();
 
@@ -118,44 +119,43 @@ router.post('/getMaintaice', async (req, res) => {
     let staffIds = [];
     let roomIds = [];
     try {
-        const requests = await mMaintenanceRequest.find().lean();;
+        const requests = await mMaintenanceRequest.find().lean();
+
         requests.forEach((each) => {
             staffIds.push(each.staffId);
             roomIds.push(each.roomId);
         });
 
-
-        // console.log({ staffIds, roomIds });roomName
-
-        const staffDetails = await mStaff.find({ _id: { $in: staffIds } }, { name: 1 });
-        const roomDetails = await mRoom.find({ _id: { $in: roomIds } }, { roomName: 1 });
-
-        // const combinedData = requests.map((request)=>{
-
-        //     {
-
-        //         request,
-        //          staff: staffDetails.find(staff => staff._id.toString() === request.staffId.toString()),
-        //         room: roomDetails.find(room => room._id.toString() === request.roomId.toString())
-        //     }
-        //     // console.log("request",request);
-
-        // })
-
-        const combinedData = requests.map((request) => ({
-            ...request, // Spread request data
-            
-            staff: staffDetails.find(staff => staff._id.toString() === request.staffId?.toString()) || null,
-            room: roomDetails.find(room => room._id.toString() === request.roomId?.toString()) || null
-        }));
-
-        console.log({ staffDetails, roomDetails });
-
-        console.log("------------");
-        console.log(combinedData);
+        const staffDetails = await mStaff.find(
+            { _id: { $in: staffIds } },
+            { name: 1 },
+            { lean: true }
+        );
+        const roomDetails = await mRoom.find(
+            { _id: { $in: roomIds } },
+            { roomName: 1 },
+            { lean: true }
+        );
 
 
-        res.status(200).json(combinedData);
+        const grpdStaffsById = _.groupBy(staffDetails, '_id');
+        const grpdRoomsById = _.groupBy(roomDetails, '_id');
+
+        requests.forEach(eachMaintenanceRequest => {
+
+            eachMaintenanceRequest['roomName'] = '';
+            if (grpdRoomsById[eachMaintenanceRequest.roomId] && grpdRoomsById[eachMaintenanceRequest.roomId].length) {
+                eachMaintenanceRequest['roomName'] = grpdRoomsById[eachMaintenanceRequest.roomId][0]['roomName'];
+            }
+
+            eachMaintenanceRequest['staffName'] = '';
+            if (grpdStaffsById[eachMaintenanceRequest.staffId] && grpdStaffsById[eachMaintenanceRequest.staffId].length) {
+                eachMaintenanceRequest['staffName']= grpdStaffsById[eachMaintenanceRequest.staffId][0]['name'];
+            }
+        });
+
+
+        res.status(200).json(requests);
     } catch (err) {
         console.error(err);
         res.status(500).json({ error: 'Internal Server Error' });
